b60f9d04a30a70ebaca8c66c7975e617baeb6b88
Harun Ar - Rasyid <32662500+Harun1804@users.noreply.github.com>
2021-10-08 21:11:26 +0700

    Template UI

---
+++ resources/views/layouts/includes/botnav.blade.php
@@ -0,0 +1,228 @@
+<div class="nav-bottom bg-white">
+    <h3 class="title-menu d-flex d-lg-none"> 
+        Menu 
+        <div class="close-menu"> <i class="flaticon-cross"></i></div>
+    </h3>
+    <div class="container d-flex flex-row">
+        <ul class="nav page-navigation page-navigation-secondary">
+            <li class="nav-item submenu active">
+                <a class="nav-link" href="#">
+                    <i class="link-icon icon-screen-desktop"></i>
+                    <span class="menu-title">Dashboard</span>
+                </a>
+                <div class="navbar-dropdown animated fadeIn">
+                    <ul>
+                        <li>
+                            <a href="{{ asset('') }}demo1/index.html">Dashboard 1</a>
+                        </li>
+                        <li>
+                            <a href="{{ asset('') }}demo2/index.html">Dashboard 2</a>
+                        </li>
+                        <li>
+                            <a href="{{ asset('') }}demo3/index.html">Dashboard 3</a>
+                        </li>
+                        <li>
+                            <a href="{{ asset('') }}demo4/index.html">Dashboard 4</a>
+                        </li>
+                        <li>
+                            <a href="{{ asset('') }}demo5/index.html">Dashboard 5</a>
+                        </li>
+                        <li>
+                            <a href="{{ asset('') }}demo6/index.html">Dashboard 6</a>
+                        </li>
+                        <li>
+                            <a href="{{ asset('') }}demo7/index.html">Dashboard 7</a>
+                        </li>
+                        <li>
+                            <a href="{{ asset('') }}demo8/index.html">Dashboard 8</a>
+                        </li>
+                        <li>
+                            <a href="{{ asset('') }}demo9/index.html">Dashboard 9</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item submenu">
+                <a class="nav-link" href="#">
+                    <i class="link-icon icon-grid"></i>
+                    <span class="menu-title">Apps</span>
+                </a>
+                <div class="navbar-dropdown animated fadeIn">
+                    <ul>
+                        <li>
+                            <a href="boards.html">Boards</a>
+                        </li>
+                        <li>
+                            <a href="projects.html">Projects</a>
+                        </li>
+                        <li>
+                            <a href="email-inbox.html">Email Inbox</a>
+                        </li>
+                        <li>
+                            <a href="email-detail.html">Email Detail</a>
+                        </li>
+                        <li>
+                            <a href="email-compose.html">Email Inbox</a>
+                        </li>
+                        <li>
+                            <a href="messages.html">Messages</a>
+                        </li>
+                        <li>
+                            <a href="conversations.html">Conversations</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item submenu">
+                <a class="nav-link" href="#">
+                    <i class="link-icon icon-disc"></i>
+                    <span class="menu-title">Finance</span>
+                </a>
+                <div class="navbar-dropdown animated fadeIn">
+                    <ul>
+                        <li>
+                            <a href="starter-template.html">Annual Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">HR Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">Finance Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">Revenue Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">IPO Report</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item submenu mega-menu dropdown">
+                <a class="nav-link" href="#">
+                    <i class="link-icon icon-film"></i>
+                    <span class="menu-title">Project</span>
+                </a>
+                <div class="navbar-dropdown animated fadeIn">
+                    <div class="col-group-wrapper row">
+                        <div class="col-group col-md-4">
+                            <div class="row">
+                                <div class="col-12">
+                                    <p class="category-heading">Basic Elements</p>
+                                    <div class="submenu-item">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <ul>
+                                                    <li><a href="#">Accordion</a></li>
+                                                    <li><a href="#">Buttons</a></li>
+                                                    <li><a href="#">Badges</a></li>
+                                                    <li><a href="#">Breadcrumbs</a></li>
+                                                    <li><a href="#">Dropdown</a></li>
+                                                    <li><a href="#">Modals</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <ul>
+                                                    <li><a href="#">Progress bar</a></li>
+                                                    <li><a href="#">Pagination</a></li>
+                                                    <li><a href="#">Tabs</a></li>
+                                                    <li><a href="#">Typography</a></li>
+                                                    <li><a href="#">Tooltip</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-group col-md-4">
+                            <div class="row">
+                                <div class="col-12">
+                                    <p class="category-heading">Advanced Elements</p>
+                                    <div class="submenu-item">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <ul>
+                                                    <li><a href="#">Datatables</a></li>
+                                                    <li><a href="#">Carousel</a></li>
+                                                    <li><a href="#">Clipboard</a></li>
+                                                    <li><a href="#">Chart.js</a></li>
+                                                    <li><a href="#">Loader</a></li>
+                                                    <li><a href="#">Slider</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <ul>
+                                                    <li><a href="#">Popup</a></li>
+                                                    <li><a href="#">Notification</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-group col-md-4">
+                            <p class="category-heading">Icons</p>
+                            <ul class="submenu-item">
+                                <li><a href="#">Flaticons</a></li>
+                                <li><a href="#">Font Awesome</a></li>
+                                <li><a class="3">Simple Line Icons</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="nav-item submenu">
+                <a class="nav-link" href="#">
+                    <i class="link-icon icon-book-open"></i>
+                    <span class="menu-title">HR</span>
+                </a>
+                <div class="navbar-dropdown animated fadeIn">
+                    <ul>
+                        <li>
+                            <a href="starter-template.html">Annual Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">HR Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">Finance Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">Revenue Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">IPO Report</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item submenu">
+                <a class="nav-link" href="#">
+                    <i class="link-icon icon-pie-chart"></i>
+                    <span class="menu-title">Revenue</span>
+                </a>
+                <div class="navbar-dropdown animated fadeIn">
+                    <ul>
+                        <li>
+                            <a href="starter-template.html">Annual Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">HR Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">Finance Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">Revenue Report</a>
+                        </li>
+                        <li>
+                            <a href="starter-template.html">IPO Report</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
