9525285b192c77e90a77fa4358feb9e7b4f588f5
Harun Ar - Rasyid <32662500+Harun1804@users.noreply.github.com>
2021-11-01 09:02:53 +0700

    Fix typo bug

--- resources/views/livewire/alat/edit.blade.php
+++ resources/views/livewire/alat/edit.blade.php
@@ -66,8 +66,8 @@
                                     <label class="custom-control-label" for="customRadio2">Hematologi</label>
                                 </div>
                                 <div class="custom-control custom-radio">
-                                    <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input" value="urinalis" wire:model="jenis" @if ($jenis == "urinalis") checked @endif>
-                                    <label class="custom-control-label" for="customRadio3">Urinalis</label>
+                                    <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input" value="urinalisis" wire:model="jenis" @if ($jenis == "urinalis") checked @endif>
+                                    <label class="custom-control-label" for="customRadio3">Urinalisis</label>
                                 </div>
                             </div>
                         </div>
